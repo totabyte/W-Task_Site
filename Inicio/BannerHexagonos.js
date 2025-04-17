82efb6c5e5f4b441b8612fc66d61b3a122ffa1e6
ISMAEL <ismaelsantanateixeira@gmail.com>
2025-04-16 23:04:41 -0300

    Alteração dos Hexágonos

--- Inicio/BannerHexagonos.js
+++ Inicio/BannerHexagonos.js
@@ -11,12 +11,12 @@ function createHexagon() {
     const banner = document.getElementById("banner");
     banner.appendChild(hex);
 
-    const size = Math.random() * 50 + 30;
+    const size = Math.random() * 50 + 35;
     hex.style.width = `${size}px`;
     hex.style.height = `${size}px`;
     hex.style.left = `${Math.random() * banner.offsetWidth}px`;
     hex.style.top = `${Math.random() * banner.offsetHeight}px`;
-    hex.style.animationDuration = `${Math.random() * 5 + 3}s, ${Math.random() * 3 + 2}s, 8s`;
+    hex.style.animationDuration = `${Math.random() * 5 + 3.5}s, ${Math.random() * 3 + 2}s, 8s`;
     hex.style.backgroundColor = `rgba(${100 + Math.random() * 100}, 0, 0, 0.7)`;
 
     setTimeout(() => hex.remove(), 8000);
