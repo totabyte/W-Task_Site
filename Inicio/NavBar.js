a0e5f43f146a7a154cce436bf67c583b2e8873f3
ISMAEL <ismaelsantanateixeira@gmail.com>
2025-04-19 07:25:45 -0300

    Conclusão do Desing da NavBar

---
+++ Inicio/NavBar.js
@@ -0,0 +1,17 @@
+function alterarSobreNos(){
+    document.getElementById("sobreNos").style.color = "#dacbb7";
+    document.getElementById("contatos").style.color = "#ffffff";
+    document.getElementById("clientes").style.color = "#ffffff";
+}
+
+function alterarContatos(){
+    document.getElementById("sobreNos").style.color = "#ffffff";
+    document.getElementById("contatos").style.color = "#dacbb7";
+    document.getElementById("clientes").style.color = "#ffffff";
+}
+
+function alterarClientes(){
+    document.getElementById("sobreNos").style.color = "#ffffff";
+    document.getElementById("contatos").style.color = "#ffffff";
+    document.getElementById("clientes").style.color = "#dacbb7";
+}
