59d85aa61aae24927a377874bed23202bb51d0d0
ISMAEL <ismaelsantanateixeira@gmail.com>
2025-04-20 23:36:25 -0300

    Continuações da colocação do portifólio

---
+++ Inicio/Portifolio.js
@@ -0,0 +1,9 @@
+function MouseNoBotaoPort(elemento){
+    elemento.style.backgroundColor = "#4c071b";
+    elemento.style.color = "#ffffff";
+}
+
+function MouseForaBotaoPort(elemento){
+    elemento.style.backgroundColor = "transparent";
+    elemento.style.color = "#000000";
+}
